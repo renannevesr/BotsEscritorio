ff97c3c98d787298aa7af5f8717bbf585eec584d
Renan Rego <renannevesr@gmail.com>
2024-05-02 08:40:05 -0300

    feat: html em port

--- precTRF5.py
+++ precTRF5.py
@@ -1,6 +1,7 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import html
 
 url = 'https://cp.trf5.jus.br/cp/cp.do'
 myobj = {'tipo': 'xmlrpvprec',
@@ -23,7 +24,20 @@ if response.status_code == 200:
      #   print(link['href'])
     url_prec=url_base_prec+prec_link
     print(url_prec)
-    response_prec = requests.get(url_prec)
-    print(response_prec.text)
+    response_prec_raw = requests.get(url_prec)
+    response_prec=html.unescape(response_prec_raw.text)
+    print(response_prec)
+    soup = BeautifulSoup(response_prec, 'html.parser')
+
+    reqte_td = soup.find_all('td')[17].get_text(strip=True)
+    proc_origin_td = soup.find_all('td')[3].get_text(strip=True)
+    precatorio_td = soup.find_all('td')[0].get_text(strip=True)
+    autuado_td = soup.find_all('td')[1].find('div', align='right').get_text(strip=True)
+
+    # Imprimindo as informações
+    print("REQTE:", reqte_td)
+    print("PROC. ORIGINÁRIO Nº:", proc_origin_td)
+    print("PRECATÓRIO:", precatorio_td)
+    print("AUTUADO EM:", autuado_td)
 else:
     print("A requisição não foi bem sucedida. Código de status:", response.status_code)
